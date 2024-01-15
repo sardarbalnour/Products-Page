c1c4d4770183650792820b3874834bafcd7315aa
Sardar Balnour <balnour.sardar@gmail.com>
2024-01-15 17:31:57 +0330

    search by name added successfully.

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const searchInput = document.getElementById("search-input");
+const products = document.querySelectorAll(".product-item");
+
+const searchHandler = (event) => {
+  const searchValue = event.target.value.toLowerCase().trim();
+
+  products.forEach((product) => {
+    const productName = product.children[1].innerText.toLowerCase();
+    console.dir(productName);
+    if (productName.includes(searchValue)) {
+      product.style.display = "block";
+    } else {
+      product.style.display = "none";
+    }
+  });
+};
+
+searchInput.addEventListener("keyup", searchHandler);
